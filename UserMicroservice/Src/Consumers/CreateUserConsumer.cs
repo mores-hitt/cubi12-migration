c0f3f42c4aa9a0a2509e92ef80f5b9b7577ffefb
mores-hitt <fernando.sanabria@alumnos.ucn.cl>
2024-12-15 12:21:19 -0300

    feat: implement user microservice rabbitmq consumers

--- UserMicroservice/Src/Consumers/CreateUserConsumer.cs
+++ UserMicroservice/Src/Consumers/CreateUserConsumer.cs
@@ -1,15 +1,36 @@
 using MassTransit;
 using Shared.Library.Messages;
+using user_microservice.Src.Repositories.Interfaces;
+using user_microservice.Src.Models;
 
 namespace user_microservice.Src.Consumers
 {
     public class CreateUserConsumer : IConsumer<UserCreatedMessage>
     {
-        public Task Consume(ConsumeContext<UserCreatedMessage> context)
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CreateUserConsumer(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task Consume(ConsumeContext<UserCreatedMessage> context)
         {
             var message = context.Message;
             Console.WriteLine($"Received message: {message.Name}");
-            return Task.CompletedTask;
+            var user = new User
+            {
+                Name = message.Name,
+                RUT = message.RUT,
+                FirstLastName = message.FirstLastName,
+                SecondLastName = message.SecondLastName,
+                Email = message.Email,
+                HashedPassword = message.Password,
+                RoleId = message.RoleId,
+                CareerId = message.CareerId,
+            };
+            await _unitOfWork.UsersRepository.Insert(user);
+
+            return;
         }
     }
 }
--- UserMicroservice/Src/Consumers/UpdatePasswordConsumer.cs
+++ UserMicroservice/Src/Consumers/UpdatePasswordConsumer.cs
@@ -1,15 +1,31 @@
 using MassTransit;
 using Shared.Library.Messages;
+using user_microservice.Src.Services.Interfaces;
+using user_microservice.Src.Repositories.Interfaces;
+using user_microservice.Src.Exceptions;
 
 namespace user_microservice.Src.Consumers
 {
     public class UpdatePasswordConsumer : IConsumer<UpdatePasswordMessage>
     {
-        public Task Consume(ConsumeContext<UpdatePasswordMessage> context)
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UpdatePasswordConsumer(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task Consume(ConsumeContext<UpdatePasswordMessage> context)
         {
             var message = context.Message;
             Console.WriteLine($"Received message: {message.Id}");
-            return Task.CompletedTask;
+            var user = await _unitOfWork.UsersRepository.GetByID(message.Id);
+            if(user is null)
+            {
+                throw new EntityNotFoundException($"User with id: {message.Id} not found");
+            }
+            user.HashedPassword = message.Password;
+            await _unitOfWork.UsersRepository.Update(user);
+            return;
         }
     }
 }
